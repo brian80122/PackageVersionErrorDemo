38fb8a1a1dd0df89265f0dc17c9dc16e291b2d9f
brian80122 <brian80122@gmail.com>
2018-10-01 23:06:48 +0800

    init project

---
+++ SomeLibrary/SomeClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace SomeLibrary
+{
+    public class SomeClass
+    {
+        private ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public SomeClass()
+        {
+            _logger.Info($"Init {nameof(SomeClass)}");   
+        }
+
+        public string Say()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ SomeWebapi/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using SomeLibrary;
+using System.Web.Http;
+
+namespace SomeWebapi.Controllers
+{
+    [RoutePrefix("api/home")]
+    public class HomeController : ApiController
+    {
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        [HttpGet]
+        [Route("index")]
+        public string Index()
+        {
+            _logger.Info("Index called");
+            return "Index";
+        }
+
+        [HttpGet]
+        [Route("say")]
+        public string Say()
+        {
+            var instance = new SomeClass();
+            return instance.Say();
+        }
+    }
+}
